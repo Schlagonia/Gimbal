3c41a6632607178e5eaa9931dfb20153a2cd06e2
HashRei <hashrei@protonmail.com>
2022-05-06 19:18:09 +0200

    Add first test batch

---
+++ hardhat.config.ts
@@ -0,0 +1,103 @@
+require('solidity-coverage')
+import * as dotenv from "dotenv";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-waffle";
+
+
+dotenv.config();
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+// task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+//   const accounts = await hre.ethers.getSigners();
+
+//   for (const account of accounts) {
+//     console.log(account.address);
+//   }
+// });
+// To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+
+const moralisKey = process.env.MORALIS_KEY || '';
+const account = process.env.PRIV_KEY || '';
+
+module.exports = {
+  solidity: {
+    version: "0.8.10",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+    },
+    rinkeby: {
+      url: `https://speedy-nodes-nyc.moralis.io/${moralisKey}/eth/rinkeby`,
+      accounts: [account]
+    },
+    localhost: {
+      url: 'http://127.0.0.1:8545/',
+      gasPrice: 225000000000,
+      timeout: 200000,
+    },
+    polygon: {
+      url: `https://speedy-nodes-nyc.moralis.io/${moralisKey}/polygon/mainnet`,
+      chainId: 137,
+      gasPrice: 50000000000,
+      accounts: [account],
+    },
+    mumbai: {
+      url: `https://speedy-nodes-nyc.moralis.io/${moralisKey}/polygon/mumbai`,
+      chainId: 80001,
+      gasPrice: 14000000000,
+      accounts: [account],
+    },
+    opera: {
+      url: `https://speedy-nodes-nyc.moralis.io/${moralisKey}/fantom/mainnet`,
+      chainId: 250,
+      gasPrice: 2000000000,
+      accounts: [account],
+    },
+    operaTestnet: {
+      url: "https://rpc.testnet.fantom.network",
+      chainId: 4002,
+      //gasPrice: 2000000000,
+      accounts: [account],
+    },
+    avalanche: {
+      url: `https://speedy-nodes-nyc.moralis.io/${moralisKey}/avalanche/mainnet`,
+      gasPrice: 35000000000,
+      chainId: 43114,
+      accounts: [account],
+    },
+    fuji: {
+      url: `https://speedy-nodes-nyc.moralis.io/${moralisKey}/avalanche/testnet`,
+      chainId: 43113,
+      gasPrice: 225000000000,
+      accounts: [account],
+    },
+    arbitrum: {
+      url: `https://speedy-nodes-nyc.moralis.io/${moralisKey}/arbitrum/mainnet`,
+      chainId: 42161,
+      gasPrice: 600000000,
+      accounts: [account],
+    },
+    arbitrumRinkebyTestnet: {
+      url: `https://speedy-nodes-nyc.moralis.io/${moralisKey}/arbitrum/testnet`,
+      chainId: 421611,
+      gasPrice: 600000000,
+      accounts: [account],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_KEY
+  }
+};
--- test/Bridgerton.test.ts
+++ test/Bridgerton.test.ts
@@ -1,18 +1,21 @@
-const solidity = require("ethereum-waffle").solidity;
-const expect = require("chai").expect;
-const use = require("chai").use;
-const ethers = require("hardhat").ethers;
-const SignerWithAddress =
-  require("@nomiclabs/hardhat-ethers/signers").SignerWithAddress;
-// const ERC20 = require("../contracts/Solmate/tokens/ERC20.sol");
+import { solidity } from "ethereum-waffle";
+import { use, expect } from "chai";
+import { ethers } from "hardhat";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 
 use(solidity);
 
-let accounts; // SignerWithAddress[]
-const stargateRouter = "0x45A01E4e04F14f7A4a6702c74187c5F6222033cd"; // Address of the router on Polygon check the hlepers to get it on mumbay and other networks
-let bridgerton; // Bridgerton contract
-let vault; // List of vaults
-let underlying; // Underlying ERC20 token for the vault
+let accounts: SignerWithAddress[]; // SignerWithAddress[]
+let bridgerton: any; // Bridgerton contract
+let vault: any; // List of vaults
+let vaultSecond: any;
+let underlying: any; // Underlying ERC20 token for the vaults
+
+const USDC_ON_POLYGON = "0x2791bca1f2de4661ed88a30c99a7a9449aa84174";
+const USDT_ON_POLYGON = "0xc2132d05d31c914a87c6611c10748aeb04b58e8f";
+const USDC_ON_AVALANCHE = "0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E";
+const STARGATEROUTER = "0x45A01E4e04F14f7A4a6702c74187c5F6222033cd"; // Address of the router on Polygon
+const ETHER = "1000000000000000000";
 
 describe("Bridgerton contract", function () {
   beforeEach(async function () {
@@ -20,45 +23,80 @@ describe("Bridgerton contract", function () {
 
     /** Deploy core contracts **/
 
-    // Deploy Bridgerton
+    // Deploy Bridgerton contract
     const Bridgerton = await ethers.getContractFactory("Bridgerton");
-    bridgerton = await Bridgerton.deploy(stargateRouter);
+    bridgerton = await Bridgerton.deploy(STARGATEROUTER);
     await bridgerton.deployed();
 
-    console.log("Bridgerton Deployed to: ", bridgerton.address);
-
-    // ===================
-    // Creation of the ERC20 token for the vault
-    const Token = await ethers.getContractFactory("ERC20");
-    const underlying = await Token.deploy();
+    // Deploy ERC20 token for the vaults
+    const Token = await ethers.getContractFactory("GT");
+    underlying = await Token.deploy(10000000000000);
     await underlying.deployed();
 
-    // underlying = new ERC20("Test Token", "TTK"); 
-    // underlying = "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174" // has to be a ERC20 
-    
-    // ===================
-    // Creation of keepers to test setVault function
-    //TODO: check the test of keepers and theri cretaion
-
-    // ===================
-    // Creation of vaults to test setVault function
-    // first they shouldn't be allowed to do call the swap fuunction
-    // but after beeing allowed it should work
-
-    // Creation of the first vault, what is a vault=> a vault is an ERC4626 contract in our case a Savor4626 contract
+    // Vault creation, a vault is an ERC4626 contract in our case a Savor4626 contract
     const Vault = await ethers.getContractFactory("SavorVault");
-    vault = await Vault.deploy(underlying, bridgerton, accounts[0].address); //  ERC20 _UNDERLYING, address _bridgerton, address _keeper by default is msg.sender
+    vault = await Vault.deploy(
+      underlying.address,
+      bridgerton.address,
+      accounts[0].address //TODO: check the test of keepers and their creation, for now accounts[0].address is enough
+    );
     await vault.deployed();
 
-    // console.log("Vault Deployed to: ", vault.address);
+    // Creation of the second vault
+    const VaultSecond = await ethers.getContractFactory("SavorVault");
+    vaultSecond = await VaultSecond.deploy(
+      underlying.address,
+      bridgerton.address,
+      accounts[0].address //TODO: check the test of keepers and their creation, for now accounts[0].address is enough
+    );
+    await vaultSecond.deployed();
 
-    //Checks
+    // Checks
     expect(bridgerton.address).to.not.equal(ethers.constants.AddressZero);
+    expect(underlying.address).to.not.equal(ethers.constants.AddressZero);
+    expect(vault.address).to.not.equal(ethers.constants.AddressZero);
+    expect(vaultSecond.address).to.not.equal(ethers.constants.AddressZero);
   });
-  describe("Transactions", function () {
+
+  describe("Main functions - Integration tests", function () {
     it("Test setVault", async () => {
-      console.log("TEST");
+      await bridgerton.setVault(vault.address);
+
+      // Checks
+      expect(await bridgerton.vaults(vault.address)).to.equal(true);
+    });
+    it("Add an assets to the Bridgerton", async () => {
+      await bridgerton.addAsset(USDC_ON_POLYGON, 1);
+      await bridgerton.addAsset(USDT_ON_POLYGON, 2); // check https://stargateprotocol.gitbook.io/stargate/developers/pool-ids for pids
+      await bridgerton.addAsset(USDC_ON_AVALANCHE, 1);
+
+      // Checks
+      expect(await bridgerton.pids(USDC_ON_POLYGON)).to.equal(1);
+      expect(await bridgerton.pids(USDT_ON_POLYGON)).to.equal(2);
+      expect(await bridgerton.pids(USDC_ON_AVALANCHE)).to.equal(1);
+    });
+
+    it("Test sweep function", async () => {
+      const initialTokenBalanceSender = await underlying.balanceOf(
+        accounts[0].address
+      );
+      const initialTokenBalanceBridgerton = await underlying.balanceOf(
+        bridgerton.address
+      );
+
+      // Transfer tokens to the bridgerton contract
+      await underlying.transfer(bridgerton.address, 100);
+      expect(await underlying.balanceOf(bridgerton.address)).to.equal(100);
+
+      await bridgerton.sweep(underlying.address);
+
+      // Checks
+      expect(await underlying.balanceOf(accounts[0].address)).to.equal(
+        initialTokenBalanceSender
+      );
+      expect(await underlying.balanceOf(bridgerton.address)).to.equal(
+        initialTokenBalanceBridgerton
+      );
     });
   });
 });
-
